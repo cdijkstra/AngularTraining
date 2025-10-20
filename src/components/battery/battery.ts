67ac368bbad906a8627ff46a4859929ef480f95a
Casper <c.d.dijkstra@hva.nl>
2025-10-20 19:56:37 +0200

    Batteries

--- src/components/battery/battery.ts
+++ src/components/battery/battery.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { signal, Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { signal } from '@angular/core';
 import { MatButton} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 
@@ -13,6 +12,7 @@ import {MatIconModule} from "@angular/material/icon";
 })
 
 export class Battery {
+  batteryName = input.required<string>();
   batteryLevel = signal(100); // Example value, replace with your logic
   fontSizeStyle = { 'font-size': '2rem' };
   today = new Date();
