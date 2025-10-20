1535c0ce9ebaa2c91d78c3685a0284777db1f151
Casper <c.d.dijkstra@hva.nl>
2025-10-20 19:23:10 +0200

    Increase & Decrease battery

--- src/components/battery/battery.ts
+++ src/components/battery/battery.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-battery',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './battery.html',
   styleUrl: './battery.scss'
 })
 
 export class Battery {
+  batteryLevel = signal(100); // Example value, replace with your logic
+  fontSizeStyle = { 'font-size': '2rem' };
+  today = new Date();
 
+  ResetBattery() {
+    this.batteryLevel.set(0)
+  }
+  IncreaseBattery(amount: number) {
+    this.batteryLevel.update(value => value + amount);
+  }
+  DecreaseBattery(amount: number) {
+    this.batteryLevel.update(value => value - amount);
+  }
 }
+
