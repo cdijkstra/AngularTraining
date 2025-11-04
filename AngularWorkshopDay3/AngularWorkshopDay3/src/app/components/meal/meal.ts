413aa361017e5bcf3a3c6879813ced0f8b725d6f
Casper <c.d.dijkstra@hva.nl>
2025-11-04 18:15:36 +0100

    Set up own project

---
+++ AngularWorkshopDay3/AngularWorkshopDay3/src/app/components/meal/meal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meal',
+  imports: [],
+  templateUrl: './meal.html',
+  styleUrl: './meal.css',
+})
+export class Meal {
+
+}
